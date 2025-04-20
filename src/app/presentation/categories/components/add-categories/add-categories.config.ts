be0f5261f3a7e5d3d01dce46fcdeabd9107395e1
Yohann Pardo <pardoyohan@gmail.com>
2025-04-20 09:44:39 -0500

    Feat: enable add category

---
+++ src/app/presentation/categories/components/add-categories/add-categories.config.ts
@@ -0,0 +1,22 @@
+export const ADD_CATEGORIES_CONFIG = Object.freeze({
+  TEXTS: {
+    CATEGORY_TITLE: 'Nombre de la categoría',
+    CATEGORY_TITLE_PLACEHOLDER: 'Escribe el categoría',
+    CATEGORY_HEADER: 'Nueva categoría',
+    DATE_LABEL: 'Asignar fecha (opcional)',
+    NO_DATE_SELECTED: 'No se ha elegido una fecha',
+  },
+  BUTTONS: {
+    ADD_CATEGORY: {
+      TEXT: 'Guardar',
+    },
+    CLOSE: {
+      TEXT: 'Cerrar'
+    }
+  },
+  ALERTS: {
+    TITLE: 'Información incompleta',
+    MESSAGE: 'Completa el nombre de la categoría antes de guardarla.',
+    BUTTON: 'Entendido',
+  },
+});
